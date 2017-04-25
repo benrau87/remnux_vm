5ebcd8f593b45fd637dbb1db412fdc839d1dbc4a
benrau87 <benrau87@users.noreply.github.com>
2017-04-25 13:42:40 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+apt-get update
+apt-get install open-vm-tools-desktop -y
+
+cd /usr/lib/vmware-tools/modules/source
+tar xf vmhgfs.tar
+grep -q d_u.d_alias vmhgfs-only/inode.c && echo "already patched" && exit 0
+sed -i -e s/d_alias/d_u.d_alias/ vmhgfs-only/inode.c
+cp -p vmhgfs.tar vmhgfs.tar.orig
+tar cf vmhgfs.tar vmhgfs-only
+vmware-config-tools.pl -d -m
